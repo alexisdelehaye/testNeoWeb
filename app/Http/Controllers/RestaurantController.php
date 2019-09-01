64c4f7cb9b51a4f4eac9f6705c2cf1bcb49a8192
Alexis Delehaye <alexis_delehaye@ens.univ-artois.fr>
2019-09-01 18:33:55 +0200

    Création du Controller Restaurant

---
+++ app/Http/Controllers/RestaurantController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Restaurant;
+use Illuminate\Http\Request;
+
+class RestaurantController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $restaurants = Restaurant::all();
+
+        return view('restaurant.index',["restaurants" => $restaurants]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $restaurant = Restaurant::find($id);
+        return view('restaurant.show',compact('restaurant'));
+    }
+}
