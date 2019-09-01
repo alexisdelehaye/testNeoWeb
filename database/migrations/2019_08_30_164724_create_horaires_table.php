c19ab6273155f2f53d3fac21c046acfe37263d70
Alexis Delehaye <alexis_delehaye@ens.univ-artois.fr>
2019-09-01 17:46:09 +0200

    Création des scripts de migration des horaires et des restaurant

---
+++ database/migrations/2019_08_30_164724_create_horaires_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHorairesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('horaires', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('Jour'); // Jour du crénaux
+            $table->string('Crénaux'); // liste des crénaux du jour
+            $table->unsignedBigInteger('restaurants_id');
+            $table->foreign('restaurants_id')->references('id')->on('restaurants');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('horaires');
+    }
+}
